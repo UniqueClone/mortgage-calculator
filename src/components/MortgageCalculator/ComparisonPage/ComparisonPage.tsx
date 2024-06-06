9a708ff15b2bac84f8bf3004f85a1805123add55
Ryan Lynch <lynchryan2@gmail.com>
2024-06-06 16:50:58 +0100

    Fixing lint issue.

--- src/components/MortgageCalculator/ComparisonPage/ComparisonPage.tsx
+++ src/components/MortgageCalculator/ComparisonPage/ComparisonPage.tsx
@@ -3,52 +3,52 @@ import { ComparisonSection } from "./ComparisonSection";
 
 interface ComparisonPageProps {}
 
-const ComparisonPage: React.FC<ComparisonPageProps> = (
-    props: ComparisonPageProps
-) => {
-    return (
-        <div>
-            <h1>Compare Mortgages</h1>
-            <ComparisonSection
-                option1={{
-                    housePrice: 440000,
-                    depositPercentage: 0.1,
-                    mortgageTerm: 35,
-                    interestRate: 3.8,
-                    fees: {
-                        valuationFee: 185,
-                        surveyFee: 500,
-                        legalFee: 3300,
-                        searchFee: 250,
-                    },
-                }}
-                option2={{
-                    housePrice: 450000,
-                    depositPercentage: 0.1,
-                    mortgageTerm: 35,
-                    interestRate: 3.8,
-                    fees: {
-                        valuationFee: 185,
-                        surveyFee: 500,
-                        legalFee: 3300,
-                        searchFee: 250,
-                    },
-                }}
-                option3={{
-                    housePrice: 460000,
-                    depositPercentage: 0.1,
-                    mortgageTerm: 35,
-                    interestRate: 3.8,
-                    fees: {
-                        valuationFee: 185,
-                        surveyFee: 500,
-                        legalFee: 3300,
-                        searchFee: 250,
-                    },
-                }}
-            />
-        </div>
-    );
-};
+const ComparisonPage: React.FC<ComparisonPageProps> = () =>
+    // props: ComparisonPageProps
+    {
+        return (
+            <div>
+                <h1>Compare Mortgages</h1>
+                <ComparisonSection
+                    option1={{
+                        housePrice: 440000,
+                        depositPercentage: 0.1,
+                        mortgageTerm: 35,
+                        interestRate: 3.8,
+                        fees: {
+                            valuationFee: 185,
+                            surveyFee: 500,
+                            legalFee: 3300,
+                            searchFee: 250,
+                        },
+                    }}
+                    option2={{
+                        housePrice: 450000,
+                        depositPercentage: 0.1,
+                        mortgageTerm: 35,
+                        interestRate: 3.8,
+                        fees: {
+                            valuationFee: 185,
+                            surveyFee: 500,
+                            legalFee: 3300,
+                            searchFee: 250,
+                        },
+                    }}
+                    option3={{
+                        housePrice: 460000,
+                        depositPercentage: 0.1,
+                        mortgageTerm: 35,
+                        interestRate: 3.8,
+                        fees: {
+                            valuationFee: 185,
+                            surveyFee: 500,
+                            legalFee: 3300,
+                            searchFee: 250,
+                        },
+                    }}
+                />
+            </div>
+        );
+    };
 
 export default ComparisonPage;
