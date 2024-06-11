541b72d60fc9b79a159366bf2af2d8da26cadc67
Ryan Lynch <lynchryan2@gmail.com>
2024-06-11 17:00:07 +0100

    Adding story for InputField.

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,10 +1,15 @@
-import { TextField } from "@fluentui/react";
+import { ITextFieldStyles, TextField } from "@fluentui/react";
 
 export interface InputFieldProps {
     label: string;
     value: number;
     onChange: (newValue: number) => void;
+    styles?: Partial<ITextFieldStyles>;
 }
+
+const defaultTextFieldStyles: Partial<ITextFieldStyles> = {
+    fieldGroup: { width: 200 },
+};
 
 export const InputField: React.FC<InputFieldProps> = (
     props: InputFieldProps
@@ -12,13 +17,14 @@ export const InputField: React.FC<InputFieldProps> = (
     return (
         <TextField
             label={props.label}
-            value={props.value.toString()}
             onChange={(_e, newValue) => {
                 if (newValue === undefined) {
                     return;
                 }
                 props.onChange(parseFloat(newValue));
             }}
+            styles={props.styles ?? defaultTextFieldStyles}
+            value={props.value.toString()}
         />
     );
 };
