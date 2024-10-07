2f78fe19cad2eb0c596d9d0c9485d0c62da78e56
Ryan Lynch <lynchryan2@gmail.com>
2024-10-07 22:28:23 +0100

    Adding first time buyer checkbox.

--- src/components/MortgageComparison/MortgageComparison.tsx
+++ src/components/MortgageComparison/MortgageComparison.tsx
@@ -1,4 +1,11 @@
-import { Icon, Link, PrimaryButton, Stack, Text } from "@fluentui/react";
+import {
+  Checkbox,
+  Icon,
+  Link,
+  PrimaryButton,
+  Stack,
+  Text,
+} from "@fluentui/react";
 import React, { useEffect } from "react";
 import { InterestRate } from "../InterestRate/InterestRate";
 import { MaxLoanInput } from "../MaxLoanInput/MaxLoanInput";
@@ -10,163 +17,187 @@ import { FeesPanel } from "../FeesPanel/FeesPanel";
 export interface MortgageComparisonProps {}
 
 export const MortgageComparison: React.FC<MortgageComparisonProps> = () => {
-    const savedMortgageDetails = localStorage.getItem("mortgageDetails");
-    const parsedMortgageDetails = JSON.parse(savedMortgageDetails ?? "{}");
+  const savedMortgageDetails = localStorage.getItem("mortgageDetails");
+  const parsedMortgageDetails = JSON.parse(savedMortgageDetails ?? "{}");
 
-    const [interestRate, setInterestRate] = React.useState<number | undefined>(
-        parsedMortgageDetails.interestRate ?? 4.0
+  const [firstTimeBuyer, setFirstTimeBuyer] = React.useState(true);
+
+  const [interestRate, setInterestRate] = React.useState<number | undefined>(
+    parsedMortgageDetails.interestRate ?? 4.0
+  );
+  const [useGlobalInterestRate, setUseGlobalInterestRate] = React.useState(
+    parsedMortgageDetails.useGlobalInterestRate ?? true
+  );
+
+  const [maxLoan, setMaxLoan] = React.useState(
+    parsedMortgageDetails.maxLoan ?? 0
+  );
+
+  const [term, setTerm] = React.useState(parsedMortgageDetails.term ?? 35);
+
+  const [fees, setFees] = React.useState<MortgageFees>(
+    parsedMortgageDetails.fees ?? {
+      valuationFee: 185,
+      surveyFee: 600,
+      // legalFee: 3382.5,
+      legalFee: 2800,
+      searchFee: 250,
+      registerOfDeedsFee: 100,
+      landRegistryFee: 975,
+    }
+  );
+
+  const [isPanelOpen, setIsPanelOpen] = React.useState(false);
+
+  const containerStackStyles = {
+    root: { alignItems: "center" },
+  };
+  const containerStackTokens = { childrenGap: 30 };
+  const comparisonStackTokens = { childrenGap: 40 };
+
+  useEffect(() => {
+    localStorage.setItem(
+      "mortgageDetails",
+      JSON.stringify({
+        fees,
+        interestRate,
+        useGlobalInterestRate,
+        maxLoan,
+        term,
+      })
     );
-    const [useGlobalInterestRate, setUseGlobalInterestRate] = React.useState(
-        parsedMortgageDetails.useGlobalInterestRate ?? true
-    );
+  }, [fees, interestRate, useGlobalInterestRate, maxLoan, term]);
 
-    const [maxLoan, setMaxLoan] = React.useState(
-        parsedMortgageDetails.maxLoan ?? 0
-    );
+  return (
+    <Stack styles={containerStackStyles} tokens={containerStackTokens}>
+      <h1>Mortgage Comparison</h1>
 
-    const [term, setTerm] = React.useState(parsedMortgageDetails.term ?? 35);
+      <Checkbox
+        label="First Time Buyer?"
+        checked={firstTimeBuyer}
+        onChange={(_e, checked) => {
+          if (checked === undefined) {
+            return;
+          }
+          setFirstTimeBuyer(checked);
+        }}
+      />
 
-    const [fees, setFees] = React.useState<MortgageFees>(
-        parsedMortgageDetails.fees ?? {
-            valuationFee: 185,
-            surveyFee: 600,
-            // legalFee: 3382.5,
-            legalFee: 2800,
-            searchFee: 250,
-            registerOfDeedsFee: 100,
-            landRegistryFee: 975,
-        }
-    );
+      <InterestRate
+        interestRate={interestRate}
+        setInterestRate={setInterestRate}
+        useGlobalInterestRate={useGlobalInterestRate}
+        setUseGlobalInterestRate={setUseGlobalInterestRate}
+      />
 
-    const [isPanelOpen, setIsPanelOpen] = React.useState(false);
+      <MaxLoanInput maxLoan={maxLoan} setMaxLoan={setMaxLoan} />
 
-    const containerStackStyles = {
-        root: { alignItems: "center" },
-    };
-    const containerStackTokens = { childrenGap: 30 };
-    const comparisonStackTokens = { childrenGap: 40 };
+      <TermInput term={term} setTerm={setTerm} />
 
-    useEffect(() => {
-        localStorage.setItem(
-            "mortgageDetails",
-            JSON.stringify({
-                fees,
-                interestRate,
-                useGlobalInterestRate,
-                maxLoan,
-                term,
-            })
-        );
-    }, [fees, interestRate, useGlobalInterestRate, maxLoan, term]);
+      <PrimaryButton onClick={() => setIsPanelOpen(true)}>
+        View/Edit Fees
+      </PrimaryButton>
 
-    return (
-        <Stack styles={containerStackStyles} tokens={containerStackTokens}>
-            <h1>Mortgage Comparison</h1>
-            <InterestRate
-                interestRate={interestRate}
-                setInterestRate={setInterestRate}
-                useGlobalInterestRate={useGlobalInterestRate}
-                setUseGlobalInterestRate={setUseGlobalInterestRate}
-            />
+      <Stack horizontal tokens={comparisonStackTokens} wrap>
+        <MortgageOption
+          fees={fees}
+          firstTimeBuyer={firstTimeBuyer}
+          id={1}
+          interestRate={interestRate}
+          useGlobalInterestRate={useGlobalInterestRate}
+          maxLoan={maxLoan}
+          term={term}
+        />
 
-            <MaxLoanInput maxLoan={maxLoan} setMaxLoan={setMaxLoan} />
+        <MortgageOption
+          fees={fees}
+          firstTimeBuyer={firstTimeBuyer}
+          id={2}
+          interestRate={interestRate}
+          useGlobalInterestRate={useGlobalInterestRate}
+          maxLoan={maxLoan}
+          term={term}
+        />
 
-            <TermInput term={term} setTerm={setTerm} />
+        <MortgageOption
+          fees={fees}
+          firstTimeBuyer={firstTimeBuyer}
+          id={3}
+          interestRate={interestRate}
+          useGlobalInterestRate={useGlobalInterestRate}
+          maxLoan={maxLoan}
+          term={term}
+        />
+      </Stack>
 
-            <PrimaryButton onClick={() => setIsPanelOpen(true)}>
-                View/Edit Fees
-            </PrimaryButton>
+      <br />
 
-            <Stack horizontal tokens={comparisonStackTokens} wrap>
-                <MortgageOption
-                    fees={fees}
-                    id={1}
-                    interestRate={interestRate}
-                    useGlobalInterestRate={useGlobalInterestRate}
-                    maxLoan={maxLoan}
-                    term={term}
-                />
+      <Text variant="medium" style={{ width: "70%" }}>
+        <strong>Disclaimer:</strong> This is a simple mortgage comparison tool
+        that calculates monthly payments based on the interest rate, loan
+        amount, and term. The numbers are all estimates based on my own
+        experience and research. Always consult with a financial advisor before
+        making any decisions.
+      </Text>
 
-                <MortgageOption
-                    fees={fees}
-                    id={2}
-                    interestRate={interestRate}
-                    useGlobalInterestRate={useGlobalInterestRate}
-                    maxLoan={maxLoan}
-                    term={term}
-                />
+      <Text variant="medium">
+        If you found this useful, consider{" "}
+        <Link
+          href="https://www.buymeacoffee.com/ryanlynch"
+          style={{ color: "rgb(33, 171, 56)" }}
+          target="_blank"
+        >
+          buying me a coffee! <Icon iconName="CoffeeScript" />{" "}
+        </Link>
+      </Text>
 
-                <MortgageOption
-                    fees={fees}
-                    id={3}
-                    interestRate={interestRate}
-                    useGlobalInterestRate={useGlobalInterestRate}
-                    maxLoan={maxLoan}
-                    term={term}
-                />
-            </Stack>
-
-            <br />
-
-            <Text variant="medium" style={{ width: "70%" }}>
-                <strong>Disclaimer:</strong> This is a simple mortgage
-                comparison tool that calculates monthly payments based on the
-                interest rate, loan amount, and term. The numbers are all
-                estimates based on my own experience and research. Always
-                consult with a financial advisor before making any decisions.
-            </Text>
-
-            <Text variant="medium">
-                If you found this useful, consider{" "}
-                <Link
-                    href="https://www.buymeacoffee.com/ryanlynch"
-                    style={{ color: "rgb(33, 171, 56)" }}
-                    target="_blank"
-                >
-                    buying me a coffee! <Icon iconName="CoffeeScript" />{" "}
-                </Link>
-            </Text>
-
-            <FeesPanel
-                fees={fees}
-                setFees={setFees}
-                isPanelOpen={isPanelOpen}
-                setIsPanelOpen={setIsPanelOpen}
-            />
-        </Stack>
-    );
+      <FeesPanel
+        fees={fees}
+        setFees={setFees}
+        isPanelOpen={isPanelOpen}
+        setIsPanelOpen={setIsPanelOpen}
+      />
+    </Stack>
+  );
 };
 
 interface MortgageOptionProps {
-    fees: MortgageFees;
-    id: number;
-    interestRate: number | undefined;
-    useGlobalInterestRate: boolean;
-    maxLoan: number;
-    term: number;
+  fees: MortgageFees;
+  firstTimeBuyer: boolean;
+  id: number;
+  interestRate: number | undefined;
+  useGlobalInterestRate: boolean;
+  maxLoan: number;
+  term: number;
 }
 
 const MortgageOption: React.FC<MortgageOptionProps> = (
-    props: MortgageOptionProps
+  props: MortgageOptionProps
 ) => {
-    const { fees, id, interestRate, useGlobalInterestRate, maxLoan, term } =
-        props;
+  const {
+    fees,
+    firstTimeBuyer,
+    id,
+    interestRate,
+    useGlobalInterestRate,
+    maxLoan,
+    term,
+  } = props;
 
-    return (
-        <Stack.Item grow>
-            <Stack horizontalAlign="center">
-                <h2>Option {id}</h2>
+  return (
+    <Stack.Item grow>
+      <Stack horizontalAlign="center">
+        <h2>Option {id}</h2>
 
-                <MortgageDetails
-                    id={id}
-                    fees={fees}
-                    interestRate={
-                        useGlobalInterestRate ? interestRate : undefined
-                    }
-                    maxLoan={maxLoan}
-                    term={term}
-                />
-            </Stack>
-        </Stack.Item>
-    );
+        <MortgageDetails
+          id={id}
+          fees={fees}
+          firstTimeBuyer={firstTimeBuyer}
+          interestRate={useGlobalInterestRate ? interestRate : undefined}
+          maxLoan={maxLoan}
+          term={term}
+        />
+      </Stack>
+    </Stack.Item>
+  );
 };
