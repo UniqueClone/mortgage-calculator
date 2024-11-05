325e7e77c91dc7a8f4265c6c44881851ebaa024d
Ryan Lynch <lynchryan2@gmail.com>
2024-11-05 23:48:10 +0000

    removing await, adding debug

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
 // import { appInsightsConfig } from "./AppInsightsConfig";
 
 const reactPlugin = new ReactPlugin();
-const x = await import.meta.env.VITE_APPINSIGHTS_CONNECTION_STRING;
+const x = import.meta.env.VITE_APPINSIGHTS_CONNECTION_STRING;
+console.log(x);
 
 export const appInsights = new ApplicationInsights({
   config: {
